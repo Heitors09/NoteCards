2c5f8d07a51212edc443449b8db9b7b862538aa3
Heitor <heitorao32@gmail.com>
2024-02-08 13:45:09 -0300

    search-submit-fixed

--- src/app.tsx
+++ src/app.tsx
@@ -65,14 +65,13 @@ export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 ">
       <img src={logo} alt="nlw-expert" />
-      <form className="w-full">
-        <input
-          type="text"
-          placeholder="Busque em suas notas..."
-          className="bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
-          onChange={handleSearch}
-        />
-      </form>
+
+      <input
+        type="text"
+        placeholder="Busque em suas notas..."
+        className="bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500 w-full"
+        onChange={handleSearch}
+      />
 
       <div className="h-px bg-slate-700" />
 
